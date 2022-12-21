a8d2ec93117a3cc8aa5ba47dcc9e350f347752d1
Kevfdezzz <84365005+Kevfdezzz@users.noreply.github.com>
2022-12-21 05:30:15 +0100

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -2,23 +2,23 @@
 let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
-let texto1 = `╭━〘𝐇𝐀𝐓𝐒𝐔𝐍𝐄 𝐌𝐈𝐊𝐔-𝐁𝐎𝐓〙━⊷
+let texto1 = `╭━〘Ҝ乇ᐯ-乃ㄖㄒ〙━⊷
 ┃ ⛥│◦➛📔ᩭ✎si deseas 🤝apoyarme mejor ayúdame y apóyame en mis cuentas oficiales de 🌆YouTube y en 👥facebook y podrías regalarme una estrella en mi cuenta de git📈
 ┃ ⛥│◦➛🍀ᩭ✎𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁
-┃ ⛥│◦➛📚ᩭ✎http://wa.me/5212411719888 
+┃ ⛥│◦➛📚ᩭ✎http://wa.me/34603022140
 ┃ ⛥│◦➛📔ᩭ✎𝚃𝚞𝚝𝚘𝚛𝚒𝚊𝚕: 𝚙𝚕𝚊𝚢
-┃ ⛥│◦➛🍀ᩭ✎https://youtu.be/6FYKkeBCTdw
+┃ ⛥│◦➛🍀ᩭ✎
 ┃ ⛥│◦➛☃️ᩭ✎𝙶𝙸𝚃 𝙲𝙻𝙾𝙽𝙴
-┃ ⛥│◦➛📚ᩭ✎https://github.com/OFC-YOVANI/HADES-BOT-MD.git
+┃ ⛥│◦➛📚ᩭ✎
 ┃ ⛥│◦➛🌱ᩭ✎𝙶𝚁𝚄𝙿𝙾 𝙳𝙴 𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺 
-┃ ⛥│◦➛🌴ᩭ✎https://www.facebook.com/groups/987464505464904
-┃ ⛥│◦➛📔ᩭ✎voy a agradecer ☘️mucho su apoyo 📚 
+┃ ⛥│◦➛🌴ᩭ✎
+┃ ⛥│◦➛📔ᩭ✎
 ┃ ⛥╰───────────
 ╰━━━━━━━━━━━──⊷`
 let buttonMessage= {
 'document': { url: `https://youtu.be/4eA5nDxPjOA` },
 'mimetype': `application/${document}`,
-'fileName': `𝐇𝐀𝐓𝐒𝐔𝐍𝐄 𝐌𝐈𝐊𝐔-𝐁𝐎𝐓`,
+'fileName': `Ҝ乇ᐯ-乃ㄖㄒ`,
 'fileLength': 99999999999999,
 'pageCount': 200,
 'contextInfo': {
