af71bd60ebe749621aec177a0c9b4e11246a1f9a
Kevfdezzz <84365005+Kevfdezzz@users.noreply.github.com>
2022-12-21 05:19:27 +0100

    Update gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -5,7 +5,7 @@ const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let text = `╭━〘𝐇𝐀𝐓𝐒𝐔𝐍𝐄 𝐌𝐈𝐊𝐔-𝐁𝐎𝐓〙━⊷ 
+let text = `╭━〘Ҝ乇ᐯ-乃ㄖㄒ〙━⊷ 
 ┃ ⛥│𝐈𝐍𝐕𝐎𝐂𝐀𝐍𝐃𝐎 𝐀𝐃𝐌𝐈𝐍𝐒 
 ┃ ⛥│${oi}
 ┃ ⛥│*𝙰𝙳𝙼𝙸𝙽𝚂:*
